6904f016c2e5f761c9f1407e095e626ba08721c0
chanwoo <chocw0402@gmail.com>
2022-03-21 18:38:50 +0900

    [WEEK3]

---
+++ src/com/company/FirstPerson.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class FirstPerson {
+    public void wakeUp() {
+    }
+
+    public void introduce() {
+    }
+
+    public void eat() {
+    }
+
+    public void walk() {
+    }
+
+    public void sleep() {
+    }
+}
